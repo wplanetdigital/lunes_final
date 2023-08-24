6ceaa2bc446af6155485d7126e94f127fab514aa
wplanetdigital <91206900+wplanetdigital@users.noreply.github.com>
2023-08-25 00:41:05 +0200

    Main Final

---
+++ scripts/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install build-essential
+sudo apt install --assume-yes git clang curl libssl-dev protobuf-compiler
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+rustup default nightly-2023-01-01
+rustup target add wasm32-unknown-unknown --toolchain nightly-2023-01-01
+cd lunes-node
+cargo build --release
